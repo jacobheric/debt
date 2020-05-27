467f798f8de3bcc43fc933b8bdeb83c3071e5c4f
Jacob Heric <jacob@jacobheric.com>
2020-05-27 09:45:13 -0400

    * initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -3,27 +3,10 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <header>
+    <div className="p-4 bg-gray-100 border-b-2 border-gray-200">
+      <h1>
+        <Link to="/">
           {siteTitle}
         </Link>
       </h1>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,55 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import { graphql } from "gatsby"
 
-const IndexPage = () => (
+export const repos = graphql`
+  query repos {
+    github {
+      search(query: "org:debtcollective", type: REPOSITORY, first: 50) {
+        repositoryCount
+        edges {
+          node {
+            ... on GitHub_Repository {
+              name
+              description
+              languages(first: 10) {
+                edges {
+                  node {
+                    name
+                  }
+                }
+              }              
+              stargazers {
+                totalCount
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO title="Home"/>
+    <h1 className="mx-5">{data.github.search.repositoryCount} Repos</h1>
+    <div className="flex flex-wrap">
+      {data.github.search.edges.map(({ node: repo }) =>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 border border-gray-400 rounded-md m-4 p-3 ">
+          <div className="font-bold">{repo.name}</div>
+          <hr className="mb-2"/>
+          <div className="text-sm mb-2">{repo.description}</div>
+          <div className="text-sm mb-2">
+            {repo.languages.edges.map(({ node: lang}) =>
+              <span className="inline-block pr-1">{lang.name}</span>)}
+          </div>
+          <div className="text-sm">★ {repo.stargazers.totalCount}</div>
+        </div>
+      )}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
